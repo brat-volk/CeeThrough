cf4ebc4928a1d2bf473bd1beb5ba374676c54df0
brat_volk <43145630+brat-volk@users.noreply.github.com>
2021-01-26 20:41:57 +0100

    aggiunto main.cpp (helper.exe)

---
+++ sc/Main.cpp
@@ -0,0 +1,44 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <iostream>
+#include <tlhelp32.h>
+
+int main(){
+
+    bool isprocessup;
+
+
+    while (1){
+
+        if(GetKeyState(VK_NEXT) & 0x8000){ //se PG_DOWN viene premuto  controllo lo stato della gui
+
+            PROCESSENTRY32 entry;
+            entry.dwSize = sizeof(PROCESSENTRY32);
+            HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+            if (Process32First(snapshot, &entry) == TRUE){
+                while (Process32Next(snapshot, &entry) == TRUE){
+                    if (stricmp(entry.szExeFile, "CeeThrough.exe") == 0){
+                        HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, entry.th32ProcessID);
+                    }
+                }
+            }
+
+            if(isprocessup = "False"){
+
+                //killo la gui
+
+            }else{
+
+                //starto la gui
+
+            }
+            CloseHandle(hProcess);//out-of-scope
+
+        }
+        if(GetKeyState(VK_PRIOR) & 0x8000){ //se PG_UP viene premuto esco dal loop
+            break;
+        }
+    }
+    CloseHandle(snapshot); //out-of-scope
+    return 0;
+}
